79c4b2f505e26adb1c67251f75401852037f1df2
mksphaninder <julienbi@unity3d.com>
2020-09-23 22:12:30 -0400

    - Added the Video Grid - Added the package uuid for generating the unique ids for the rooms. - Initialized the video from the browser using the file Script.js

--- server.js
+++ server.js
@@ -5,10 +5,19 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 
+// setting up default view engine.
 app.set("view engine", "ejs");
 
+app.use(express.static("public"));
+
+// setting up uuid for generating the unique id's
+const { v4: uuidv4 } = require("uuid");
+
 app.get("/", (req, res) => {
-  res.render("room");
+  res.redirect(`/${uuidv4()}`);
 });
 
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
 server.listen(3030); // port
